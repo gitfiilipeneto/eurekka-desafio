4b36017360b51a243832441b857b00bce45c0033
Filipe <filipenetopires@hotmail.com>
2021-06-09 22:39:51 -0300

    New APi added, fix tomorrow the request

--- src/api/Controllers.js
+++ src/api/Controllers.js
@@ -1,39 +1,38 @@
-import React, { useState, useEffect } from 'react'
-import API from './api'
+import React, { useState, useEffect } from "react";
+import API from "./api";
 
-const Controllers = () =>  {
-const [tmdb, setTmdb] = useState([])
+const Controllers = () => {
+  const [tmdb, setTmdb] = useState([]);
+  const [omdb, setOmdb] = useState([]);
 
-useEffect(() => {
-    API.getTMDB()
-    .then(tmdb =>setTmdb(tmdb))
+  useEffect(() => {
+    API.getTMDB().then((tmdb) => setTmdb(tmdb));
+  }, []);
+  useEffect(() => {
+    API.getOMDB().then(omdb => setOmdb(omdb));
+  }, []);
 
-}, [])
-console.log(tmdb)
-let moviesTittle = tmdb.title //apply destructurng method for each .item on tmdb
-let moviesDescription = tmdb.overview
-let moviesRating = tmdb.vote_average
-let moviesImgDefaultUrl = "https://image.tmdb.org/t/p/original/"
-let moviesFrontCover = moviesImgDefaultUrl+tmdb.poster_path
-let moviesBackCover = moviesImgDefaultUrl+tmdb.backdrop_path
+  console.log(`${omdb} OMDB`);
+  let moviesTittle = tmdb.title; //apply destructurng method for each .item on tmdb
+  let moviesDescription = tmdb.overview;
+  let moviesRating = tmdb.vote_average;
+  let moviesImgDefaultUrl = "https://image.tmdb.org/t/p/original/";
+  let moviesFrontCover = moviesImgDefaultUrl + tmdb.poster_path;
+  let moviesBackCover = moviesImgDefaultUrl + tmdb.backdrop_path;
 
-return(
+  return (
     <>
-        {moviesTittle}
-        {moviesDescription}
-        <p>
-        tmdb Average Rating {moviesRating}
-        </p>
-        <img src={moviesFrontCover} alt="movie cover"></img>
-        <img src={moviesBackCover} alt="movie cover"></img>
-         
+      {moviesTittle}
+      {moviesDescription}
+      <p>tmdb Average Rating {moviesRating}</p>
+      <img src={moviesFrontCover} alt="movie cover"></img>
+      <img src={moviesBackCover} alt="movie cover"></img>
     </>
     //map the response to dinamically generate the movies infos
     //uses material UI to style the cards with movies infos
-)
-
-}
+  );
+};
 
 //use this to default application controller
 
-export default Controllers
+export default Controllers;
--- src/api/api.js
+++ src/api/api.js
@@ -1,17 +1,26 @@
-
-let tmdb = undefined
-
-class API{
-    static getTMDB(){
-        if(!tmdb){
-            tmdb = fetch('https://api.themoviedb.org/3/movie/550?api_key=a341a9d7cb2bd4eba1b729d6e957cbf9') // api Example, single movie req i'll read the doc for more
-
-            .then( response => response.json())
-        }
-        return tmdb
-        .then (tmdb => tmdb)
+let tmdb = undefined;
+let omdb = undefined;
+class API {
+  static getTMDB() {
+    if (!tmdb) {
+      tmdb = fetch(
+        "https://api.themoviedb.org/3/movie/550?api_key=a341a9d7cb2bd4eba1b729d6e957cbf9")
+        // api Example, single movie req i'll read the doc for more
+        .then((response) => response.json());
     }
-    
-} // call others API here
+    return tmdb
+    .then((tmdb) => tmdb);
+  }
 
-export default API
+  static getOMDB() {
+    if (!omdb) {
+      omdb = fetch("http://www.omdbapi.com/?i=tt3896198&apikey=40e4e39d")
+      .then((omdbResponse) => omdbResponse.json());
+    }
+    return omdb
+    .then((omdb) => omdb);
+  }
+} // call others API here
+// https://api.thetvdb.com/swagger
+
+export default API;
